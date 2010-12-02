8452b7d423b1cf81e62dba339dea45e8ebc08ac3
Gabor Szabo <gabor@szabgab.com>
2010-12-02 17:52:06 +0200

    move news items to separate news.yml file

--- lib/PEG.pm
+++ lib/PEG.pm
@@ -87,77 +87,20 @@ get '/benefits' => sub {
     };
 };
 
-my @news = (
-	{
-		date => '2010.11.29',
-		text => '<a href="http://www.summersault.com/">Summersault</a> provides an initial <a href="/sponsors">sponsorship</a> to PEG',
-		title => 'Summersault sponsors the Perl Ecosystem Group',
-		author => 'Gabor Szabo',
-	},
-	{
-		date => '2010.11.10',
-		text => '<a href="http://www.oqapi.com/en">Oqapi</a> agrees to become member of the Perl Ecosystem Group',
-		title => 'Oqapi joins the Perl Ecosystem Group',
-		author => 'Gabor Szabo',
-	},
-	{
-		date => '2010.11.09',
-		text => 'PEG announces it will support the Perl teams participating on the <a href="http://www.plat-forms.org/">Plat_Forms contest</a>. See the <a href="http://szabgab.com/blog/2010/11/sponsoring-plat-forms-contest-teams.html">blog</a> about it.',
-		title => 'The Perl Ecosystem Group and the Plat_Forms competition',
-		author => 'Gabor Szabo',
-	},
-	{
-		date => '2010.11.06',
-		text => '<a href="http://www.dijkmat.nl/">Dijkmat BV</a> becomes the first Bronze <a href="/members">member</a> of the Perl Ecosystem Group.',
-		title => 'Dijkmat joins the Perl Ecosystem Group',
-		author => 'Gabor Szabo',
-	},
-	{
-		date => '2010.11.06',
-		text => 'Gabor Szabo from the Perl Ecosystem Group together with several members of the <a href="http://www.sppn.nl/">Dutch Perl Mongers</a> participate at <a href="http://www.t-dose.org/">T-Dose</a></h3> in Eindhoven, The Netherlands with a Perl booth.',
-		title => 'Perl Ecosystem Group at T-Dose',
-		author => 'Gabor Szabo',
-	},
-	{
-		date => '2010.11.05',
-		text => 'Blog entries announcing the establishment of the Perl Ecosystem Group in <a href="http://szabgab.com/blog/2010/11/perl-ecosystem-group.html">English</a> and in <a href="http://reneeb-perlblog.blogspot.com/2010/11/perl-ecosystem-group.html">German</a>.',
-		title => 'Blogs about the Perl Ecosystem Group',
-		author => 'Gabor Szabo',
-	},
-	{
-		date => '2010.11.02',
-		text => '<a href="http://www.raz.co.il/">Raz Information Systems</a></h3> became a <a href="/members">member</a>.',
-		title => 'Raz joins the Perl Ecosystem Group',
-		author => 'Gabor Szabo',
-	},
-	{
-		date => '2010.10.26',
-		text => '<a href="http://www.columbusprogroup.com">Columbus Pro Group</a> was added as a <a href="/members">member</a>.',
-		title => 'Columbus Pro Group joins the Perl Ecosystem Group',
-		author => 'Gabor Szabo',
-	},
-	{
-		date => '2010.10.21',
-		text => '<a href="http://bruck.co.il/">Uri Bruck</a> added as the first <a href="/members">member</a>.',
-		title => 'Uri Bruck joins the Perl Ecosystem Group',
-		author => 'Gabor Szabo',
-	},
-	{
-		date => '2010.10.20',
-		text => 'The web site of the Perl Ecosystem Group was opened to public. We started to contact companies to sponsor our activities.',
-		title => 'Perl Ecosystem Group website is open.',
-		author => 'Gabor Szabo',
-        },
-);
 
 get '/news' => sub {
+    my $news = _read_news();
     template 'news', { 
       title        => 'News',
       subtitle     => 'News',
       description  => 'News about the Perl Ecosystem Group',
-      news         => \@news,
+      news         => $news,
     };
 };
+
+sub _read_news {
+    YAML::LoadFile(path config->{appdir}, 'data', 'news.yml');
+}
 
 get '/rss' => sub {
 
@@ -182,7 +125,8 @@ get '/rss' => sub {
             updateBase       => "1901-01-01T00:00+00:00",
         }
     );
-    foreach my $n (@news) {
+    my $news = _read_news();
+    foreach my $n (@$news) {
         my $text = $n->{text};
 	$text =~ s{"/}{"$url/}g;
         $rss->add_item(
