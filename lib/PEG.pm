22e1ad42c365da7066979aeecc1048e598355434
Gabor Szabo <szabgab@gmail.com>
2011-03-06 02:24:00 -0800

    add another rss feed listing all the events

--- lib/PEG.pm
+++ lib/PEG.pm
@@ -81,14 +81,21 @@ get '/rss/news' => sub {
     return _rss('news');
 };
 
+get '/rss/events' => sub {
+    return _rss('events');
+};
+
 sub _rss {
+    my $name = shift;
+
     my $rss  = XML::RSS->new( version => '1.0' );
     my $year = 1900 + (localtime)[5];
-    my $url  = 'http://perl-ecosystem.org';
+    my $base = "http://perl-ecosystem.org";
+    my $url  = "$base/$name";
 
     $rss->channel(
-        title       => "Perl Ecosystem Group",
-        link        => "$url/",
+        title       => "Perl Ecosystem Group $name",
+        link        => $url,
         description => 'Bridging the gap between business and the open source Perl community',
         dc          => {
             language  => 'en-us',
@@ -103,13 +110,21 @@ sub _rss {
         }
     );
 
-    foreach my $n (@{ _content()->{news}{news} }) {
-        my $text = $n->{text};
-        $text =~ s{"/}{"$url/}g;
+    foreach my $n (@{ _content()->{$name}{$name} }) {
+        my $text = $n->{text} || '';
+        $text =~ s{"/}{"$base/}g;
+
+        if ($name eq 'events') {
+            if ($n->{days} == 1) {
+                $text = "On $n->{date}";
+            } else {
+                $text = "starting on $n->{date} for $n->{days} days";
+            }
+        }
 
         $rss->add_item(
             title       => decode( 'utf-8', $n->{title} ),
-            link        => $url . ( $n->{permalink} || '/news' ),
+            link        => $url . ( $n->{permalink} || '' ),
             description => decode( 'utf-8', $text ),
             dc          => {
                 creator  => $n->{author},
